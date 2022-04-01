f0c200645ac517961e40a2b311e84dc3812ba9e2
Nick <nikita.nikolenko@gmail.com>
2022-04-01 18:13:39 +0100

    Updates

---
+++ precache-manifest.1431dd52223568a7bbc40ad56311395c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a91995d56cef6b3adca",
+    "url": "/portfolio-website/static/css/main.fbbe07fb.chunk.css"
+  },
+  {
+    "revision": "6a91995d56cef6b3adca",
+    "url": "/portfolio-website/static/js/main.6a91995d.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/portfolio-website/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "1414f231cbe53a0386a2",
+    "url": "/portfolio-website/static/js/2.1414f231.chunk.js"
+  },
+  {
+    "revision": "1f867a9be4b4515590ac",
+    "url": "/portfolio-website/static/js/runtime~main.1f867a9b.js"
+  },
+  {
+    "revision": "eeb95c5701fa2071f50f20ef9eae31b0",
+    "url": "/portfolio-website/index.html"
+  }
+];
