5c73b197cbb9ddbde13da2da896bd2e464821b19
Nick <nikita.nikolenko@gmail.com>
2022-05-12 13:59:24 +0100

    Updates

---
+++ precache-manifest.5ab1d26d99ce5747ba3c93ca5a6a6a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bcac3f705111b031284",
+    "url": "/static/css/main.4adee461.chunk.css"
+  },
+  {
+    "revision": "2bcac3f705111b031284",
+    "url": "/static/js/main.2bcac3f7.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "f4be803d699506a46d1a",
+    "url": "/static/js/2.f4be803d.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "220da063dad5df57f00fdad30584f015",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66e87b5d72aced67c98fba5f289dcedb.js"
+  "/precache-manifest.5ab1d26d99ce5747ba3c93ca5a6a6a4d.js"
 );
 
 workbox.clientsClaim();
