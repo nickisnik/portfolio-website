ae118599f25a302f9e399be19434b365c9733a4c
Nick <nikita.nikolenko@gmail.com>
2022-05-23 20:51:42 +0100

    Added animation to project cards and other

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import './Projects.css';
 
+
 function Projects(props) {
     return (
         <section className='project-list'>
             <div className='project'>
                 <header className='project-header'>
-                    <img className='project-screen' src="/Hoodify_Screenshot.png" alt="" />
+                    <img className='project-screen' src="/Hoodify_Screenshot.png" alt="Hoodify" />
                 </header>
                 <section className='project-details'>
                     <span className="project-title">Hoodify</span>
                     <p className='project-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt exercitationem cumque quia ipsum!</p>
                     <div className="project-links">
+                        <a href=""><img src="/GitHub-Mark-Light-32px.png"></img></a>
                         <a href="https://hoodify.netlify.app/"><button>Demo</button></a>
-                        <a href=""><button>Code</button></a>
+                    </div>
+                </section>
+            </div>
+            <div className='project'>
+                <header className='project-header'>
+                    <img className='project-screen' src="/MemoryCard_screen.png" alt="Memory card" />
+                </header>
+                <section className='project-details'>
+                    <span className="project-title">Memory Card</span>
+                    <p className='project-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt exercitationem cumque quia ipsum!</p>
+                    <div className="project-links">
+                        <a href=""><img src="/GitHub-Mark-Light-32px.png"></img></a>
+                        <a href="https://hoodify.netlify.app/"><button>Demo</button></a>
                     </div>
                 </section>
             </div>
@@ -25,8 +39,8 @@ function Projects(props) {
                     <span className="project-title">Hoodify</span>
                     <p className='project-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt exercitationem cumque quia ipsum!</p>
                     <div className="project-links">
+                        <a href=""><img src="/GitHub-Mark-Light-32px.png"></img></a>
                         <a href="https://hoodify.netlify.app/"><button>Demo</button></a>
-                        <a href=""><button>Code</button></a>
                     </div>
                 </section>
             </div>
@@ -38,21 +52,8 @@ function Projects(props) {
                     <span className="project-title">Hoodify</span>
                     <p className='project-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt exercitationem cumque quia ipsum!</p>
                     <div className="project-links">
+                        <a href=""><img src="/GitHub-Mark-Light-32px.png"></img></a>
                         <a href="https://hoodify.netlify.app/"><button>Demo</button></a>
-                        <a href=""><button>Code</button></a>
-                    </div>
-                </section>
-            </div>
-            <div className='project'>
-                <header className='project-header'>
-                    <img className='project-screen' src="/Hoodify_Screenshot.png" alt="" />
-                </header>
-                <section className='project-details'>
-                    <span className="project-title">Hoodify</span>
-                    <p className='project-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt exercitationem cumque quia ipsum!</p>
-                    <div className="project-links">
-                        <a href="https://hoodify.netlify.app/"><button>Demo</button></a>
-                        <a href=""><button>Code</button></a>
                     </div>
                 </section>
             </div>
